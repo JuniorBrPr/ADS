6bf671e81a79675d6b3a172abba1c8d47d9324b5
Jimi Bakare <jimi.bakare@hva.nl>
2023-09-15 18:22:53 +0200

    Verder gewerkt aan de Wagon files

---
+++ Assignment1/src/main/java/models/FreightWagon.java
@@ -0,0 +1,15 @@
+package models;
+
+public class FreightWagon extends Wagon {
+
+    public int maxWeight;
+
+    public FreightWagon(int wagonId, int maxWeight) {
+        super(wagonId);
+        this.maxWeight = maxWeight;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+}
